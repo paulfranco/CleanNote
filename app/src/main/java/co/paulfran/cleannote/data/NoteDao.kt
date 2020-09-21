7c86de950d8b76cb6eb2865745e64233b10421ad
Paul Franco <mail@paulfran.co>
2020-09-21 07:56:52 -0700

    Create Note Dao

---
+++ app/src/main/java/co/paulfran/cleannote/data/NoteDao.kt
@@ -0,0 +1,18 @@
+package co.paulfran.cleannote.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM note_table ORDER BY id ASC")
+    fun getAllData(): LiveData<List<NoteData>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(noteData: NoteData)
+
+}
