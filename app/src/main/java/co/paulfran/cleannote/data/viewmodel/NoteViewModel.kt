841c99f169938ccd1dad72b9e2f90faacd5baa28
Paul Franco <mail@paulfran.co>
2020-09-21 09:07:51 -0700

    created NoteViewModel

---
+++ app/src/main/java/co/paulfran/cleannote/data/viewmodel/NoteViewModel.kt
@@ -0,0 +1,33 @@
+package co.paulfran.cleannote.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import co.paulfran.cleannote.data.NoteDatabase
+import co.paulfran.cleannote.data.models.NoteData
+import co.paulfran.cleannote.data.repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application): AndroidViewModel(application) {
+
+    private val noteDao = NoteDatabase.getDatabase(
+        application
+    ).noteDao()
+    private val repository: NoteRepository
+
+    private val getAllData: LiveData<List<NoteData>>
+
+    init {
+        repository = NoteRepository(noteDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(noteData: NoteData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(noteData)
+        }
+    }
+
+}
