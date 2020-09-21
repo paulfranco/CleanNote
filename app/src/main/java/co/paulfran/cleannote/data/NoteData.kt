bdf88b7a594ecc5860918b0ebdd9d928ac46d829
Paul Franco <mail@paulfran.co>
2020-09-21 07:51:04 -0700

    Create the Entity Class NoteData

---
+++ app/src/main/java/co/paulfran/cleannote/data/NoteData.kt
@@ -0,0 +1,13 @@
+package co.paulfran.cleannote.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+data class NoteData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var priority: Priority,
+    var description: String
+)
