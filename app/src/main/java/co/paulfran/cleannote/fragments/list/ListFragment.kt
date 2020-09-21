5647109174dd8fbe7c8015f4d22942cd07354492
Paul Franco <mail@paulfran.co>
2020-09-21 04:22:16 -0700

    List Fragment Menu

--- app/src/main/java/co/paulfran/cleannote/fragments/list/ListFragment.kt
+++ app/src/main/java/co/paulfran/cleannote/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package co.paulfran.cleannote.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import co.paulfran.cleannote.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -28,7 +26,14 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        // set menu
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
 
