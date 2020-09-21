787d62f63a0ab9b1c78ce340b29d8228520a0567
Paul Franco <mail@paulfran.co>
2020-09-21 04:41:36 -0700

    Update Fragment Menu

--- app/src/main/java/co/paulfran/cleannote/fragments/update/UpdateFragment.kt
+++ app/src/main/java/co/paulfran/cleannote/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package co.paulfran.cleannote.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import co.paulfran.cleannote.R
 
 
@@ -15,9 +13,16 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // set menu
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
 
 }
