a2849f300fbd112c146b02467c31dd394be64512
Paul Franco <mail@paulfran.co>
2020-09-21 04:32:26 -0700

    Add Fragment menu

--- app/src/main/java/co/paulfran/cleannote/fragments/add/AddFragment.kt
+++ app/src/main/java/co/paulfran/cleannote/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package co.paulfran.cleannote.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import co.paulfran.cleannote.R
 
 
@@ -15,8 +13,15 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // set menu
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
 
